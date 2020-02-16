04949f30e62da1d9b3624a166ef77da2dc7772c1
VilmaU <57656566+VilmaU@users.noreply.github.com>
2020-02-16 16:14:57 +0200

    Update roman_test.py

--- roman_test.py
+++ roman_test.py
@@ -4,8 +4,8 @@ from roman import to_arabic
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        res = to_arabic("C")
-        self.assertEqual(100, res)
+        res = to_arabic("CI")
+        self.assertEqual(101, res)
 
         res = to_arabic("MM")
         self.assertEqual(2000, res)
